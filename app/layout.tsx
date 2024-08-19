c05aa7fbb27fafeedaf4e617b80a2a40de1eaf0d
tushar-2658 <36257197+tushar-2658@users.noreply.github.com>
2024-08-19 01:35:22 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { FirebaseAppProvider } from 'reactfire';
 
 const inter = Inter({ subsets: ["latin"] });
 
